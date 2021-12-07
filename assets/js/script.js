5b420188b8d165d27916f225ebf949452c244597
dcrlsn <c4r150n@gmail.com>
2021-12-07 11:01:08 -0500

    filled out object, display property flip

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,50 +30,105 @@ function getWeatherData() {
     });
   ;
 };
-// theaudiodb.com/api/v1/json/2/search.php?s=coldplay
+
 function getSongRecommendation(data) {
   var recommendation;
   console.log(data.weather[0].main);
   if (Math.floor(data.main.temp === 69)) recommendation = [{
-    artist: 'Rick+Astley',
+    artist: '112884',
     spotifyID: '0gxyHStUsqpMadRV0Di1Qt'
   }];
+  else if (Math.floor(data.weather[0].temp === 42)) recommendation = [{
+    artist: 'Noisestorm',
+    spotifyID: '2BuSNpxpMGJGiAAWJYJT2t'
+  }]
   else {
     switch (data.weather[0].main) {
       case "Clouds": recommendation = [{
-        artist: 'Rick+Astley',
-        spotifyID: '0gxyHStUsqpMadRV0Di1Qt'
+        artist: '134694',
+        spotifyID: '2qnpHrOzdmOo1S4ox3j17x'
+      }, {
+        artist: '126251',
+        spotifyID: '5vSQUyT33qxr1xAX2Tkf3A'
+      }, {
+        artist: '111614',
+        spotifyID: '6FXMGgJwohJLUSr5nVlf9X'
       }];
         break;
-      case "Thunderstorm": recommendation;
+      case "Thunderstorm": recommendation = [{
+        artist: '119231',
+        spotifyID: '711MCceyCBcFnzjGY4Q7Un'
+      }, {
+        artist: '111619',
+        spotifyID: '7w29UYBi0qsHi5RTcv3lmA'
+      }, {
+        artist: '111641',
+        spotifyID: '6kBDZFXuLrZgHnvmPu9NsG'
+      }, {
+        artist: '117618',
+        spotifyID: '4frXpPxQQZwbCu3eTGnZEw'
+      }, {
+        artist: '112125',
+        spotifyID: '05fG473iIaoy82BF1aGhL8'
+      }, {
+        artist: '136341',
+        spotifyID: '6MoQZOH2KnQrJhVtO9VoXC'
+      }];
         break;
-      case "Drizzle": recommendation;
+      case "Drizzle": recommendation = [{
+        artist: '166553',
+        spotifyID: '6mrOjLZyPub9LcecUarcMD'
+      }, {
+        artist: '148190',
+        spotifyID: '5NtMqQLCzdVvL7F8vFp3zM'
+      }];
         break;
       case "Rain": recommendation = [{
-        artist: 'Rick+Astley',
-        spotifyID: '0gxyHStUsqpMadRV0Di1Qt'
+        artist: '126777',
+        spotifyID: '0xBkYJzwFzcIYev4fOkvk0'
+      }, {
+        artist: '149696',
+        spotifyID: '2sSGPbdZJkaSE2AbcGOACx'
       }];
         break;
-      case "Snow": recommendation;
+      case "Snow": recommendation = [{
+        artist: '143813',
+        spotifyID: '65dGLGjkw3UbddUg2GKQoZ'
+      }, {
+        artist: '119636',
+        spotifyID: '4uFZsG1vXrPcvnZ4iSQyrx'
+      }, {
+        artist: '126761',
+        spotifyID: '4bbjivSh1oG4NOc7uYHfw5'
+      }, {
+        artist: '154185',
+        spotifyID: '4iom7VVRU6AHRIu1JUXpLG'
+      }];
         break;
-      case "Ash" || "Tornado": recommendation = [{
-        artist: 'Imagine+Dragons',
+      case "Ash" || "Tornado": recommendation = [{ //so people seek shelter
+        artist: '114415',
         spotifyID: '53XhwfbYqKCa1cC15pYq2q'
       }, {
-        artist: 'Nickelback',
+        artist: '111365',
         spotifyID: '6deZN1bslXzeGvOLaLMOIF'
       }];
         break;
-      case "Clear": recommendation;
+      case "Clear": recommendation = [{
+        artist: '111555',
+        spotifyID: '5nPOO9iTcrs9k6yFffPxjH'
+      }, {
+        artist: '115062',
+        spotifyID: '7Gh6fW49xfXLP8DTWaLVJP'
+      }];
         break;
       case "Mist" || "Haze" || "Fog": recommendation = [{
-        artist: 'Snoop+Dogg',
+        artist: '112059',
         spotifyID: '7hJcb9fa4alzcOq3EaNPoG'
       }, {
-        artist: 'Willie+Nelson',
+        artist: '111363',
         spotifyID: '5W5bDNCqJ1jbCgTxDD0Cb3'
       }, {
-        artist: 'Jimi+Hendrix',
+        artist: '111468',
         spotifyID: '776Uo845nYHJpNaStv1Ds4'
       }];
         break;
@@ -85,7 +140,8 @@ function getSongRecommendation(data) {
     }
   }
   var randArtist = recommendation[Math.floor(Math.random() * recommendation.length)]
-  var apiUrl = `https://theaudiodb.com/api/v1/json/2/search.php?s=${randArtist.artist}`
+  var apiUrl = `https://theaudiodb.com/api/v1/json/2/artist.php?i=${randArtist.artist}`
+  console.log(apiUrl)
 
   fetch(apiUrl)
     .then(function (response) {
@@ -103,19 +159,20 @@ function getSongRecommendation(data) {
   ;
 };
 
-//<iframe src="https://open.spotify.com/embed/artist/0gxyHStUsqpMadRV0Di1Qt?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-
-
-//<iframe src="https://open.spotify.com/embed/artist/7hJcb9fa4alzcOq3EaNPoG?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
 function displaySongRecommendation(data, spotifyID) {
   artist = data.artists[0]
-  var artistBannerDiv = document.createElement('div');
-  artistBannerDiv.classList = 'has-text-centered';
-  songRecommendation.prepend(artistBannerDiv);
-  var artistBanner = document.createElement('img');
-  artistBanner.src = `${artist.strArtistBanner}`;
-  artistBannerDiv.appendChild(artistBanner);
+  currentWeather.style.display = flex;
+  songRecommendation.style.display = block;
+  if (artist.strArtistBanner) {
+    var artistBannerDiv = document.createElement('div');
+    artistBannerDiv.classList = 'has-text-centered';
+    songRecommendation.prepend(artistBannerDiv);
+    var artistBanner = document.createElement('img');
+    artistBanner.src = `${artist.strArtistBanner}`;
+    artistBannerDiv.appendChild(artistBanner);
+  };
   songRecommendationName.textContent = artist.strArtist;
+
   if (artist.strBiographyEN) songRecommendationBio.textContent = artist.strBiographyEN;
   else songRecommendationBio.textContent = '';
 
